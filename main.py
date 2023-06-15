584211af381384c3dc59951411ed8af71e8a0541
Hamza-Ahmed-Siddiqui <princehamzaahmed9@gmail.com>
2023-06-15 06:57:48 -0700

    working on sqlAlchemy

--- main.py
+++ main.py
@@ -1,6 +1,25 @@
 from flask import Flask,render_template
+from flask_sqlalchemy import SQLAlchemy
+from flask.globals import request
+
+# import pymysql
+# pymysql.install_as_MySQLdb()
+
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:@localhost/codeworld'
+
+db = SQLAlchemy(app)
+
+# id , name,phone_num,msg,date,email
+class Contact(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(80), unique=False, nullable=False)
+    phone_num = db.Column(db.String(13), unique=True, nullable=False)
+    msg = db.Column(db.String(120), unique=False, nullable=False)
+    date = db.Column(db.String(12), unique=False, nullable=False)
+    email = db.Column(db.String(20), unique=True, nullable=False)
+
 
 @app.route("/")
 def home():
@@ -16,8 +35,18 @@ def about():
     return render_template('about.html')
 
 
-@app.route("/contact")
+@app.route("/contact", methods = ['GET','POST'])
 def contact():
+    if(request.method=='POST'):
+        
+        name = request.form.get('name')
+        email = request.form.get('email')
+        phone = request.form.get('phone_num')
+        message = request.form.get('message')
+        
+    
+    # db.session.add()
+    # db.session.commit()
     return render_template('contact.html')
 
 app.run(debug=True)
